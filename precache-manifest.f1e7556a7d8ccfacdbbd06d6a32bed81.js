19f0677147c790df3c345d0184c87e6abd0f551e
Patryk Omiotek <patryk.omiotek@gmail.com>
2019-07-29 18:07:15 +0200

    Updates

---
+++ precache-manifest.f1e7556a7d8ccfacdbbd06d6a32bed81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed2b092f656fb9f19b315ec45d43c9c",
+    "url": "/sr-hello-app/index.html"
+  },
+  {
+    "revision": "69480a1e2b6c8909a5df",
+    "url": "/sr-hello-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/sr-hello-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "69480a1e2b6c8909a5df",
+    "url": "/sr-hello-app/static/js/main.70062a4b.chunk.js"
+  },
+  {
+    "revision": "0633d7efcfdf7523d7fa",
+    "url": "/sr-hello-app/static/js/runtime~main.4de95b12.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sr-hello-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
